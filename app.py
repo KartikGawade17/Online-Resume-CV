21ff7508ccc3e0c3482826fb5be6d1acf3133475
KartikGawade17 <157332771+KartikGawade17@users.noreply.github.com>
2024-06-05 09:54:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from PIL import Image
 #income and expense tracker using machine learning or deep learing
 
 current_dir = Path("D:\online cv").parent if "D:\online cv" in locals() else Path.cwd()
-#css_file = current_dir / "main.css"
+css_file = current_dir / "main.css"
 resume_file = current_dir / "CV.pdf"
 profile_pic = current_dir / "profilepic.jpg"
 
