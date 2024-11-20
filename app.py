65cbd1570ee8e4c8577ea5b4a147436fb8de646e
Kartik Gawade <157332771+KartikGawade17@users.noreply.github.com>
2024-11-20 19:05:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from PIL import Image
 current_dir = Path("D:\resumeonline") if Path("D:\resumeonline").exists() else Path.cwd()
 
 css_file = current_dir/"main.css"
-resume_file = current_dir/assets/"resume.pdf"
-profile_pic_path = current_dir/assets/"hero.jpg"
+resume_file = current_dir/"resume.pdf"
+profile_pic_path = current_dir/"hero.jpg"
 
 PAGE_TITLE = "Digital CV | Kartik Gawade"
 PAGE_ICON = ":wave:"
